4f9be6abd656e921f869e58833d55848161691f3
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-05-11 10:14:42 -0300

    Feat: Add file env

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from "react";
 import CardInput from "./components/CardInput/CardInput";
 import CardCapitais from "./components/CardCapitais/CardCapitais";
-import Input from "./components/input/input";
+import Input from "./components/Input/Input";
 
 const App = () => {
+  console.log(import.meta.env.VITE_WEATHER_API);
+
   return (
-    <React.Fragment>
+    <main>
       <h1 className="title">Previsão do tempo</h1>
       <CardInput></CardInput>
       <CardCapitais />
       <Input />
-    </React.Fragment>
+    </main>
   );
 };
 
